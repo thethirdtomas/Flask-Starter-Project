e764bcc73d8daabadcb28002232d466804fa3916
Tomas Torres <tomas3torres@live.com>
2018-12-17 16:19:39 -0600

    init commit

---
+++ webapp/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from webapp.config import Config
+
+
+db = SQLAlchemy() # Database
+bcrypt = Bcrypt() # Encyption
+
+from flaskblog.main.routes import main
+from flaskblog.errors.handlers import errors 
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+ 
+    app.register_blueprint(main)
+    app.register_blueprint(errors)
+
+    return app
---
+++ webapp/config.py
@@ -0,0 +1,4 @@
+
+class Config:
+   SECRET_KEY = 'fgfdsgmdfgio4454ikmdfs32i4m234m342'
+   SQLALCHEMY_DATABASE_URI = ''
