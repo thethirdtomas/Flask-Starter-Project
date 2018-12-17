813050dc5344dfc6ccd8721c75576da3299dd8cd
Tomas Torres <tomas3torres@live.com>
2018-12-17 16:34:44 -0600

    flash error

--- webapp/errors/handlers.py
+++ webapp/errors/handlers.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash
 
 errors = Blueprint('errors', __name__)
 
 @errors.app_errorhandler(404)
 def error_404(error):
+    flash('404', 'danger')
     return render_template('errors/404.html'), 404
